dfba946113a7ee6096efb52594e14906f28c82f7
Gustavo Glass <71805078+Gus-22@users.noreply.github.com>
2024-03-27 10:56:28 -0300

    Add files via upload

--- Reconhecimento_ultralytic.py
+++ Reconhecimento_ultralytic.py
@@ -2,62 +2,73 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
+# Importação das funções e objectos necessários da biblioteca ultralítica
 from ultralytics.utils.checks import check_imshow
 from ultralytics.utils.plotting import Annotator, colors
 
-from collections import defaultdict
+from collections import defaultdict  # Importando defaultdict do módulo collections
 
-track_history = defaultdict(lambda: [])
-model = YOLO("yolov8n.pt")
-names = model.model.names
+track_history = defaultdict(lambda: []) # Criando um defaultdict para armazenar o histórico de rastreamento dos objetos detectados
 
-#video_path = "Teste.mp4"
-cap = cv2.VideoCapture(0)
-assert cap.isOpened(), "Error reading video file"
+model = YOLO("yolov8n.pt") # Inicializando o modelo de detecção de objetos YOLO com pesos pré-treinados
+names = model.model.names  # Carregando os nomes das classes do modelo YOLO
 
+#video_path = "Reconhecimento_ultralytics\Teste.mp4" # Caminho para o arquivo de vídeo de entrada
+#cap = cv2.VideoCapture(video_path)# Abrindo o arquivo de vídeo
+cap = cv2.VideoCapture(0)#usando a webcam pra capturar imagem
+assert cap.isOpened(), "Error reading video file" # Assertiva para verificar se o arquivo de vídeo foi aberto com sucesso
+
+# Recuperando as propriedades do vídeo: largura, altura e frames por segundo (fps)
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-result = cv2.VideoWriter("object_tracking.avi",
-                       cv2.VideoWriter_fourcc(*'mp4v'),
+# Criando um objeto VideoWriter para escrever o vídeo processado com as detecções
+result = cv2.VideoWriter("Reconhecimenot_ultralytic.avi",
+                       cv2.VideoWriter_fourcc(*'mp4v'),# Codec usado para escrever o vídeo
                        fps,
-                       (w, h))
+                       (w, h))# Tamanho do quadro
 
-while cap.isOpened():
-    success, frame = cap.read()
-    if success:
-        results = model.track(frame, persist=True, verbose=False)
-        boxes = results[0].boxes.xyxy.cpu()
+while cap.isOpened(): # Looping através de cada quadro do vídeo
+    success, frame = cap.read() # Lendo um quadro do vídeo
+    if success: # Se o quadro for lido com sucesso
+        results = model.track(frame, persist=True, verbose=False)  # Detectando e rastreando objetos no quadro usando o modelo YOLO
+        boxes = results[0].boxes.xyxy.cpu() # Extraindo caixas delimitadoras dos objetos detectados
 
-        if results[0].boxes.id is not None:
+        if results[0].boxes.id is not None:  # Se os IDs dos objetos estiverem disponíveis
 
-            # Extract prediction results
+            # Extraindo classe, ID de rastreamento e pontuação de confiança dos objetos detectados
             clss = results[0].boxes.cls.cpu().tolist()
             track_ids = results[0].boxes.id.int().cpu().tolist()
             confs = results[0].boxes.conf.float().cpu().tolist()
 
-            # Annotator Init
+            # Inicializando o objeto Annotator para desenhar caixas delimitadoras e rótulos
             annotator = Annotator(frame, line_width=2)
 
+            # Iterando através de cada objeto detectado
             for box, cls, track_id in zip(boxes, clss, track_ids):
+                # Desenhando caixa delimitadora e rótulo para o objeto detectado
                 annotator.box_label(box, color=colors(int(cls), True), label=names[int(cls)])
 
-                # Store tracking history
+                # Armazenando o histórico de rastreamento do objeto detectado
                 track = track_history[track_id]
                 track.append((int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)))
-                if len(track) > 30:
+                if len(track) > 30: # Removendo a posição mais antiga se o histórico exceder um certo comprimento
                     track.pop(0)
 
-                # Plot tracks
+                # Desenhando trilhas do objeto no quadro
                 points = np.array(track, dtype=np.int32).reshape((-1, 1, 2))
-                cv2.circle(frame, (track[-1]), 7, colors(int(cls), True), -1)
-                cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2)
-
-        result.write(frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+                cv2.circle(frame, (track[-1]), 7, colors(int(cls), True), -1) # Desenhando círculo na posição atual
+                cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2) # Desenhando polilinha conectando posições anteriores
+        
+        cv2.imshow("detections", frame)
+        
+        result.write(frame) # Escrevendo o quadro anotado no arquivo de vídeo de saída
+        if cv2.waitKey(1) & 0xFF == ord("q"): # Verificando se a tecla 'q' foi pressionada para sair do loop
             break
     else:
-        break
+        break # Interrompendo o loop se o final do vídeo for alcançado
 
+# Liberando os objetos de escrita e captura de vídeo
 result.release()
 cap.release()
+# Fechando todas as janelas do OpenCV
 cv2.destroyAllWindows()
